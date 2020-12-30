f984b5290f7633389f383b78a123571d7e60a448
bisper1 <bisper@naver.com>
2020-12-30 14:23:48 +0900

    STS 에서 수정한 내용을 소스트리에서 commit 하기.

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sts Git Test 1");
+		System.out.println("STS Git Test 2");
 	}
 
 }
